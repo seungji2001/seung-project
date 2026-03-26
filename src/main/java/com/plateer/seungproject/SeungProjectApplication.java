83ac958918f75b6ad3b8e02877abe0019ad86064
seungjibaek <seungjibaek@Seungjiui-MacBookAir.local>
2026-03-26 14:13:28 +0900

    first commit

---
+++ src/main/java/com/plateer/seungproject/SeungProjectApplication.java
@@ -0,0 +1,13 @@
+package com.plateer.seungproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeungProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeungProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plateer/seungproject/SeungProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plateer.seungproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeungProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
